757b0053c226dd68b5d877083e1d9e98939e2575
master7720 <thomasajameslei@gmail.com>
2023-08-07 00:57:59 +0000

    thx auto

--- src/client/scala/com/example/ExampleModClient.scala
+++
@@ -1,9 +0,0 @@
-package com.example
-
-import net.fabricmc.api.ClientModInitializer
-
-class ExampleModClient extends ClientModInitializer {
-	override def onInitializeClient(): Unit = {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
